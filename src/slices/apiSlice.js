361f65fc4f4ebf18c4f155f407f74c2e7c68d190
CysecGhost <shahidwork58@gmail.com>
2025-12-25 00:56:07 +0500

    Connected backend

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.REACT_APP_BACKEND_URL,
+  baseUrl: import.meta.env.VITE_API_URL,
   credentials: "include",
 });
 
